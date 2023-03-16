f155c8d5afb151c7839327df890eaea40c6d8ae1
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-03-16 21:47:21 +0530

    Added changes

--- src/main/java/com/cms/student/StudentServiceApplication.java
+++ src/main/java/com/cms/student/StudentServiceApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableEurekaClient
 public class StudentServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(StudentServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StudentServiceApplication.class, args);
+    }
 
 }
