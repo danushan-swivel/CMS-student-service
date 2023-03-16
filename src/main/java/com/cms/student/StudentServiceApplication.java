3740670d52e8297aa4322f0bbd32c24506769c4d
DanushanKanagasingam <danushan@swivelgroup.com.au>
2023-03-16 22:46:16 +0530

    Added changes

--- src/main/java/com/cms/student/StudentServiceApplication.java
+++ src/main/java/com/cms/student/StudentServiceApplication.java
@@ -2,9 +2,10 @@ package com.cms.student;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-//@EnableEurekaClient
+@EnableEurekaClient
 public class StudentServiceApplication {
 
     public static void main(String[] args) {
